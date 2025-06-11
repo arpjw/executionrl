6b08c15547023e136acae5bd4de32a7305ae092a
arya <aryasomu18@gmail.com>
2025-06-10 17:37:54 -0700

    Create test_env.cpp

---
+++ tests/test_env.cpp
@@ -0,0 +1,16 @@
+#include "../include/env/MarketEnvironment.hpp"
+#include <cassert>
+#include <iostream>
+
+int main() {
+    MarketEnvironment env;
+    env.reset();
+    auto state = env.getState();
+    assert(state.size() == 3);
+
+    double reward = env.step(1);  // buy
+    assert(!env.isDone());
+
+    std::cout << "test_env passed.\n";
+    return 0;
+}
